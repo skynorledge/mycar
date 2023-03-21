ffa6d457f5a310ac5bd5e456e4d6900d6a7d844a
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2023-03-21 09:42:43 +0000

    [add]carモデル、テーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_21_090809) do
+ActiveRecord::Schema.define(version: 2023_03_21_092423) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,19 @@ ActiveRecord::Schema.define(version: 2023_03_21_090809) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "cars", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "maker_id"
+    t.integer "aero_maker_id"
+    t.string "title", null: false
+    t.text "body", null: false
+    t.string "maker_comment"
+    t.string "aero_maker_comment"
+    t.string "car_model", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
