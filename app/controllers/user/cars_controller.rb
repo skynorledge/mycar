b9787eb36234149a21fadba651160d694d92b151
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2023-03-22 17:04:34 +0000

    アクション、変数の定義を追加

--- app/controllers/user/cars_controller.rb
+++ app/controllers/user/cars_controller.rb
@@ -1,13 +1,56 @@
 class User::CarsController < ApplicationController
+
+  #before_action :authenticate_user!
+
   def new
+
+    @car = Car.new
+
   end
 
   def edit
+
+    @car = Car.find(params[:id])
+
+  end
+
+  def create
+
+    @car = Car.new(car_params)
+
+    @car.save
+
+    redirect_to car_path(@car)
+
+  end
+
+  def update
+
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+    @car.save
+
+    redirect_to car_path(@car.id)
+
   end
 
   def index
+
+    @cars = Car.all
+
   end
 
   def show
+
+    @car = Car.find(params[:id])
+
   end
+
+  private
+
+    def car_params
+      params.require(:car).permit(:user_id,:maker_id,:aero_maker_id,:car_image,
+      :title,:body,:maker_comment,:aero_maker_comment,:car_model)
+    end
+
 end
