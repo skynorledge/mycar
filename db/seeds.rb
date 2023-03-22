7b7244cf50fad8b9a5da72deb0055fdce33f5e90
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2023-03-22 19:31:30 +0000

    メーカー、エアロメーカーリスト追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,27 @@ Admin.create!(
     email: 'yk@mycar.com',
     password: '111111',
 )
+
+Maker.create!([
+    {maker_name: "TOYOTA"},
+    {maker_name: "NISSAN"},
+    {maker_name: "HONDA"},
+    {maker_name: "MAZDA"},
+    {maker_name: "SUBARU"},
+    {maker_name: "MITSUBISHI"},
+    {maker_name: "SUZUKI"},
+    {maker_name: "その他"},
+    {maker_name: "なし"},
+])
+
+AeroMaker.create!([
+    {aero_maker_name: "admiration"},
+    {aero_maker_name: "AIMGAIN"},
+    {aero_maker_name: "AMS"},
+    {aero_maker_name: "ARTISAN SPIRITS"},
+    {aero_maker_name: "TOMS"},
+    {aero_maker_name: "TRD"},
+    {aero_maker_name: "CRAVE"},
+    {aero_maker_name: "その他"},
+    {aero_maker_name: "なし"},
+])
