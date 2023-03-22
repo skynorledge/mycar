b0e052eccd9c06d1925d50b304bab1aaaa03a21e
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2023-03-22 17:03:28 +0000

    アソシエーションの追加

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,12 @@ class Car < ApplicationRecord
 
   has_one_attached :car_image
 
+  belongs_to :user
+  belongs_to :maker
+  belongs_to :aero_maker
+
+  has_many :post_tags, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
 end
