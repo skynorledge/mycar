8c9da0bb95a9d1114ec64d72b4d1bd7c04891b1d
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2023-03-26 13:00:18 +0000

    keyにprofile_imageを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:introduction,:is_deleted])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:introduction,:is_deleted,:profile_image])
   end
 
 
