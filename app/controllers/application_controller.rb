d3f232198bc2a6a709878be2e4b2883919682de0
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2023-03-30 17:24:15 +0000

    permitにemailを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:introduction,:is_deleted,:profile_image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:introduction,:is_deleted,:profile_image,:email])
   end
 
   #def configure_permitted_parameters
