4e3fac0936ff920534bb2dab187681f3b3469269
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2023-03-30 18:50:25 +0000

    create,destroyアクションを追加

--- app/controllers/user/likes_controller.rb
+++ app/controllers/user/likes_controller.rb
@@ -1,4 +1,22 @@
 class User::LikesController < ApplicationController
-  def index
+
+  def create
+    car = Car.find(params[:car_id])
+    like = current_user.likes.new(car_id: car.id)
+    like.save
+    redirect_to car_path(car)
   end
+
+  def destroy
+    car = Car.find(params[:car_id])
+    like = current_user.likes.find_by(car_id: car.id)
+    like.destroy
+    redirect_to car_path(car)
+  end
+
+  def like_params
+    params.require(:like).permit(:user_id,:car_id)
+  end
+
+
 end
