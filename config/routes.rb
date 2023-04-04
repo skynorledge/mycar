b75286995946841f9c119fc9ec7ec84bf187f44b
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2023-04-04 13:44:26 +0000

    post_userのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -23,11 +23,14 @@ Rails.application.routes.draw do
 
   scope module: :user do
 
+    #get '/post_user' => 'infos#post_user'
+    #get '/post_user/:id' => 'infos#post_user'
     get '/mypage' => "infos#show"
     patch '/mypage' => 'infos#update'
     get '/info/edit' => "infos#edit"
     get '/confirm' => 'infos#confirm'
     patch '/withdrawal' => 'infos#withdrawal'
+    get '/post_user/:id' => 'infos#post_user'
     get "liked_cars", to: "infos#liked_cars"
 
     resources :cars,only: [:create,:destroy,:new,:edit,:update,:index,:show] do
