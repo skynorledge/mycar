d6f75cb697b09be7fa54c6a0c33ff215d05b6cd3
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2023-04-15 17:27:49 +0000

    不要箇所の削除

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,25 +1,7 @@
 class HomesController < ApplicationController
   def top
-    # if params[:maker_id] && params[:maker_id] != "8"
-    #   @cars = Car.where(maker_id: params[:maker_id])
-    # elsif params[:maker_id] == "8"
-    #   @cars = Car.where.not(maker_id: [1, 2, 3, 4, 5, 6, 7])
-    # else
-    #   @cars = Car.all
-    # end
 
     @cars = Car.all
 
-    # if maker = Maker.find_by(maker_name: "その他")
-    #   @other_cars = maker.cars
-    # end
-
-    # # その他以外のメーカーの Car レコードを取得する
-    # @cars = Car.where.not(maker: Maker.where(maker_name: ["TOYOTA", "NISSAN", "HONDA", "MAZDA", "SUBARU", "MITSUBISHI", "SUZUKI"]))
-
-    # # 検索フォームが使用された場合、検索条件に合致する Car レコードを取得する
-    # if params[:search]
-    #   @cars = @cars.where('name LIKE ?', "%#{params[:search]}%")
-    # end
   end
 end
