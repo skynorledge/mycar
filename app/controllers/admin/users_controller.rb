2b3c880ac67e173ac9ed03274378e54944ae9d56
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2023-04-15 13:38:00 +0000

    変数の設定

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,31 @@
 class Admin::UsersController < ApplicationController
+
   def index
+
+    @users = User.all
+
   end
 
   def show
+
+    @user = User.find(params[:id])
+
   end
+
+  def update
+
+    @user = User.find(params[:id])
+    @user.update(is_deleted: true)
+    flash[:notice] = "退会処理を実行しました"
+    redirect_to admin_users_path
+
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:introduction,:email,:password,:password_confirmation,
+      :profile_image,:is_deleted)
+    end
+
 end
